8f957c9efed2db5a9b8160dccaccd00d4a5a5ede
Таня <tatyanababosh@gmail.com>
2021-03-24 17:37:16 +0300

    Индивидуальное

---
+++ calc4.py
@@ -0,0 +1,53 @@
+#simple div
+
+str_command = input("Please type command a + b or a-b: ")
+str_command.replace(' ', '')
+
+first_op = tuple('^')
+second_op = tuple('*/')
+third_op = ('+', '-')
+supported_op = first_op + second_op + third_op
+digits_chars = tuple(map(str, range(10))) + tuple('.,-')
+
+str_A = ''
+str_B = ''
+actions = []
+
+d = dict()
+d['opr'] = 'None'
+d['val'] = ''
+actions.append(d)
+
+for i, letter in enumerate(str_command):
+	if letter in supported_op and (i > 0) and actions[-1].get('val') != '':
+		actions.append({'opr': letter,
+						'val': ''})
+	elif letter in digits_chars:
+		actions[-1]['val'] = actions[-1].get('val') + letter
+
+for action in actions:
+	action['val'] = float(action.get('val'))
+
+
+
+result = None
+
+for action in actions:
+	if type(result) == str:
+		break
+
+	var_A = result
+	var_B = action.get('val')
+	operation = action.get('opr')
+	if operation in '+-*/':
+		if var_B == 0 and operation == '/':
+			result = 'Inf'
+		else:
+			result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
+	elif operation == '^':
+		result = var_A ** var_B
+	else:
+		result = var_A
+		result = var_B
+
+print("Result: " + str(result))
